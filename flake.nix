66873e185aff12aa40e8cb18f1517ecc6c8f0571
Tejas Agarwal <tejasagarwalbly@gmail.com>
2022-02-15 14:40:55 +0530

    added nix stuff

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in
+        rec {
+          # `nix develop`
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ 
+              rustc 
+              cargo
+              rustfmt
+              clippy
+              rls
+              nodejs
+              openssl
+              pkg-config
+            ];
+            
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            PKG_CONFIG_PATH  = "${pkgs.openssl.dev}/lib/pkgconfig";
+          };
+        }
+    );
+}
